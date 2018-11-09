c39688ebe6e290ebe044d7a6ad30025312e45c28
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-09 15:43:49 -0500

    Adding laser toggling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lasers: '',
+      instruction: 'Activate'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.toggleLasers();
+  }
+
+  toggleLasers() {
+    this.setState(state => ({
+      lasers: state.lasers ? '' : 'pew pew!',
+      instruction: state.instruction === 'Activate' ? 'Deactivate' : 'Activate'
+    }));
+
+    console.log(this.state.lasers);
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          {this.state.lasers}
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button type="button" onClick={() => this.handleClick()}>
+            {`${this.state.instruction} lasers`}
+          </button>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,48 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function ActionLink() {
+    function handleClick(e) {
+        e.preventDefault();
+        console.log("The link was clicked!");
+    }
+
+    return (
+        <a href="https://google.com" onClick={handleClick}>
+            Click Me
+        </a>
+    )
+}
+
+class Toggle extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isToggleOn: true
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        
+        //  reference the current state in callback parameter
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }))
+    }
+
+    render() {
+
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        )
+
+    }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
